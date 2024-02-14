2c1f89f3147668a33d81da95e0eb8ae07e5847d0
juanmaferreira <eljuanchi16@gmail.com>
2024-02-14 17:11:53 -0300

    Actividad 8

--- cypress/e2e/actividad08.cy.js
+++ cypress/e2e/actividad08.cy.js
@@ -3,17 +3,18 @@ describe('Actividad complementaria 8', () => {
     // URL https://pushing-it.onrender.com/api/register
     // body {username, password, gender, year, month, day}
     // Agregar aserciones para verificar status y body
-        it('Deberia registrarse en PushingIT de forma satisfactoria', () => {
-            const username = 'usuario' + Math.floor(Math.random() * 1000)
-            const password = '123456!'
-            const gender = "Male"
-            const day = '10'
-            const month = 'October'
-            const year = "1980"
-            const baseUrl = 'https://pushing-it.onrender.com';
-            let token;
-    
-            cy.request({ method: "POST",
+    it('Deberia registrarse en PushingIT de forma satisfactoria', () => {
+        const username = 'usuario' + Math.floor(Math.random() * 1000)
+        const password = '123456!'
+        const gender = "Male"
+        const day = '10'
+        const month = 'October'
+        const year = "1980"
+        const baseUrl = 'https://pushing-it.onrender.com';
+        let token;
+
+        cy.request({
+            method: "POST",
             url: `${baseUrl}/api/register`,
             headers: {
                 "authorization": `Bearer ${token}`
@@ -28,8 +29,9 @@ describe('Actividad complementaria 8', () => {
                 "year": year,
 
             },
-            }).then(respuesta =>{
-            //asercioines correspondientes del body y del status
-            })
+        }).then(respuesta => {
+            console.log(respuesta.body);
+            expect(respuesta.status).to.be.equal(201);
         })
     })
+})
